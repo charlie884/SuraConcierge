d5d7555db06787ff4c953ecd63d737fb08371e86
carlos <carlos.sanabria@emerald.studio>
2018-09-12 19:10:04 -0500

    dongrade plugin notification and remove plug badge

--- plugins/cordova-plugin-app-event/src/ios/AppDelegate+APPAppEvent.h
+++ plugins/cordova-plugin-app-event/src/ios/AppDelegate+APPAppEvent.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015 by appPlant UG. All rights reserved.
+ * Copyright (c) 2013-2017 by appPlant GmbH. All rights reserved.
  *
  * @APPPLANT_LICENSE_HEADER_START@
  *
@@ -21,28 +21,10 @@
  * @APPPLANT_LICENSE_HEADER_END@
  */
 
-package de.appplant.cordova.plugin.localnotification;
+#import "AppDelegate.h"
 
-import de.appplant.cordova.plugin.notification.Notification;
+extern NSString* const UIApplicationRegisterUserNotificationSettings;
 
+@interface AppDelegate (APPAppEvent)
 
-/**
- * The clear intent receiver is triggered when the user clears a
- * notification manually. It un-persists the cleared notification from the
- * shared preferences.
- */
-public class ClearReceiver extends de.appplant.cordova.plugin.notification.ClearReceiver {
-
-    /**
-     * Called when a local notification was cleared from outside of the app.
-     *
-     * @param notification
-     *      Wrapper around the local notification
-     */
-    @Override
-    public void onClear (Notification notification) {
-        super.onClear(notification);
-        LocalNotification.fireEvent("clear", notification);
-    }
-
-}
+@end
